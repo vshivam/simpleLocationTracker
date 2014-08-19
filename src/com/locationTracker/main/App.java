6e92473ef61797deade263401c576fc10c5bcbb2
Shivam <shivam.bitsgoa@gmail.com>
2014-08-19 13:24:10 +0800

    update values

--- src/com/locationTracker/main/App.java
+++ src/com/locationTracker/main/App.java
@@ -13,7 +13,7 @@ public class App extends Application {
 	public static int SYNC_DATA_FREQUENCY = 1000 * 60 * 10;
 	public static String LATITUDE_KEY = "LATITUDE";
 	public static String LONGITUDE_KEY = "LONGITUDE";
-	private static String IP_ADDRESS = "vshivam.spectralsun.com";
+	private static String IP_ADDRESS = "";
 	public static String ACCURACY_KEY = "ACCURACY";
 	public static String PROVIDER_KEY = "PROVIDER";
 	public static int LOCATION_ACCURACY = 50;
